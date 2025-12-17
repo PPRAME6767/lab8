f525e0b655d006baf1fe9cd4058b5778a48554cc
PPRAME6767 <nidmci9@gmail.com>
2025-12-18 00:14:10 +0700

    lab8_3 completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,50 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+using namespace std;
+#include <string>
+//รับค่าอายุ
+int main(){
+    int age;
+    double tall;
+    double bounty;
+    cout << "Enter your age: ";
+    cin  >> age ;
+    if (age <= 25){
+        cout << "Enter your height: ";
+        cin >> tall;
+        if (tall <  100){
+            cout << "Your character = Chopper";
+        }
+        else if(tall < 180 ){
+            cout << "Your character = Usopp";
+        } 
+        else {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if (bounty > 1100000000){
+                cout << "Your character = Zoro";
+            }
+            else {
+                cout << "Your character = Sanji";    
+            }
+
+        }
+
+
+    }
+    else if (age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if (bounty > 500000000){
+            cout << "Your character = Jinbe";
+        }
+        else {
+            cout << "Your character = Franky";
+        }
+    }
+        else {
+            cout << "Your character = Brook";
+        }
+}
